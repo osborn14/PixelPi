918e4a66ddcc58c37532d01fd4fc2598f478459f
Robert Benjamin Osborne <Robert@Roberts-MacBook-Air-2.local>
2018-08-09 22:43:41 -0400

    Comenting out twisted code

--- Application/Client.py
+++ Application/Client.py
@@ -34,83 +34,83 @@ def prepareArrayforDisplay(array):
         return array
     
 
-
-def displayLights():
-#    display_mode_list.append(Mode([29, 29, 128]))
-#    display_mode_list.append(Mode([0, 0, 0]))
-    while True:
-        for d in display_mode_list:
-            if type(d) == Mode:
-                DEVICE_RUNNER.displayLights(d.rgb_values)
-                
-            elif type(d) == Timer:
-                if d.activated:
-                    DEVICE_RUNNER.displayLights(d.rgb_values)
-                else:
-                    ## Untested code that get the current time to compare it to the user's desired task activation time.
-                    ## If the current time matchs the activation time, the mode is activated
-                    current_datetime = datetime.datetime.now()
-                    current_hour = int(current_datetime.hour)
-                    current_minute = int(current_datetime.minute)
-                    current_day = str(current_datetime.day)
-                    current_date = datetime.date.today()
-                    day_of_the_week = calendar.day_name[current_date.weekday()]
-                    if day_of_the_week == d.day and current_hour == d.hour and current_minute == d.minute:
-                        d.activated = True
-   
-                    # displayFromJsonArray(server_message['display colors'])
-        
-            elif type(d) == Sparkle:
-                DEVICE_RUNNER.displayLights(d.getCurrentRgbValues())
+#
+# def displayLights():
+# #    display_mode_list.append(Mode([29, 29, 128]))
+# #    display_mode_list.append(Mode([0, 0, 0]))
+#     while True:
+#         for d in display_mode_list:
+#             if type(d) == Mode:
+#                 DEVICE_RUNNER.displayLights(d.rgb_values)
+#
+#             elif type(d) == Timer:
+#                 if d.activated:
+#                     DEVICE_RUNNER.displayLights(d.rgb_values)
+#                 else:
+#                     ## Untested code that get the current time to compare it to the user's desired task activation time.
+#                     ## If the current time matchs the activation time, the mode is activated
+#                     current_datetime = datetime.datetime.now()
+#                     current_hour = int(current_datetime.hour)
+#                     current_minute = int(current_datetime.minute)
+#                     current_day = str(current_datetime.day)
+#                     current_date = datetime.date.today()
+#                     day_of_the_week = calendar.day_name[current_date.weekday()]
+#                     if day_of_the_week == d.day and current_hour == d.hour and current_minute == d.minute:
+#                         d.activated = True
+#
+#                     # displayFromJsonArray(server_message['display colors'])
+#
+#             elif type(d) == Sparkle:
+#                 DEVICE_RUNNER.displayLights(d.getCurrentRgbValues())
 
             #DEVICE_RUNNER.displayLights(d.rgb_values)
 ##        time.sleep(0.05)
         
-class MyClientProtocol(WebSocketClientProtocol):
-
-    def onConnect(self, response):
-        print("Server connected: {0}".format(response.peer))
-
-    def onOpen(self):
-        print("WebSocket connection open.")
-
-        ## Send server some basic details about the device upon connecting
-        device_name = settings_file.device_name
-        device_code = settings_file.device_code
-        device_type = settings_file.device_type
-        device_location = "temp"
-        self.sendMessage(json.dumps({'cmd' : 'REGISTER DEVICE', 'device_name':device_name ,'device code': device_code, 'device_type': device_type}).encode('utf8'))
-
-
-    def onMessage(self, payload, isBinary):
-        print("Message receicved!")
-        if isBinary:
-            print("Binary message received: {0} bytes".format(len(payload)))
-        else:
-            try:
-                print(payload.decode('utf-8'))
-                server_message = json.loads(payload.decode('utf-8'))
-                print(type(server_message))
-                ## Consider ids for removing orders
-                ## Or, send a remove all command and send all again
-
-                if server_message['cmd'] == 'DISPLAY':
-                    ## Put the display mode into the proper category
-                    if server_message['mode'] == 'simple':
-                        for m in display_mode_list:
-                            if type(m)  == Mode:
-                                display_mode_list.remove(m)
-                        display_mode_list.append(Mode(prepareArrayforDisplay(server_message['rgb array'])))
-                    elif server_message['mode'] == 'timer':
-                        display_mode_list.append(Timer(server_message['timer day'], server_message['timer hour'], server_message['timer minute'], prepareArrayforDisplay(server_message['rgb array'])))
-                    display_queue.put(payload)
-                    print("Text message received: {0}".format(payload.decode('utf8')))
-            except:
-                print("Server sent invalid json!")    
-
-    def onClose(self, wasClean, code, reason):
-        print("WebSocket connection closed: {0}".format(reason))
-        sys.exit()
+# class MyClientProtocol(WebSocketClientProtocol):
+#
+#     def onConnect(self, response):
+#         print("Server connected: {0}".format(response.peer))
+#
+#     def onOpen(self):
+#         print("WebSocket connection open.")
+#
+#         ## Send server some basic details about the device upon connecting
+#         device_name = settings_file.device_name
+#         device_code = settings_file.device_code
+#         device_type = settings_file.device_type
+#         device_location = "temp"
+#         self.sendMessage(json.dumps({'cmd' : 'REGISTER DEVICE', 'device_name':device_name ,'device code': device_code, 'device_type': device_type}).encode('utf8'))
+#
+#
+#     def onMessage(self, payload, isBinary):
+#         print("Message receicved!")
+#         if isBinary:
+#             print("Binary message received: {0} bytes".format(len(payload)))
+#         else:
+#             try:
+#                 print(payload.decode('utf-8'))
+#                 server_message = json.loads(payload.decode('utf-8'))
+#                 print(type(server_message))
+#                 ## Consider ids for removing orders
+#                 ## Or, send a remove all command and send all again
+#
+#                 if server_message['cmd'] == 'DISPLAY':
+#                     ## Put the display mode into the proper category
+#                     if server_message['mode'] == 'simple':
+#                         for m in display_mode_list:
+#                             if type(m)  == Mode:
+#                                 display_mode_list.remove(m)
+#                         display_mode_list.append(Mode(prepareArrayforDisplay(server_message['rgb array'])))
+#                     elif server_message['mode'] == 'timer':
+#                         display_mode_list.append(Timer(server_message['timer day'], server_message['timer hour'], server_message['timer minute'], prepareArrayforDisplay(server_message['rgb array'])))
+#                     display_queue.put(payload)
+#                     print("Text message received: {0}".format(payload.decode('utf8')))
+#             except:
+#                 print("Server sent invalid json!")
+#
+#     def onClose(self, wasClean, code, reason):
+#         print("WebSocket connection closed: {0}".format(reason))
+#         sys.exit()
 
 
 
