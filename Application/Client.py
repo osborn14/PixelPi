ee3b27cd308567d53e09ddb80e379758e43051e9
Robert Benjamin Osborne <Robert@Roberts-MacBook-Air-2.local>
2018-08-09 22:30:39 -0400

    Moved Settings.py

--- Application/Client.py
+++ Application/Client.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 # from autobahn.asyncio.websocket import WebSocketClientProtocol
 
-from Application import Settings
+from Application.Settings import Settings
 
 ## Create our settings file as a global variable
 settings = Settings()
