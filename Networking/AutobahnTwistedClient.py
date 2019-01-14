4819bbd54f94668818b6d95d1e5f85eebcc50239
Robert B Osborne <osborn14@purdue.edu>
2019-01-13 21:25:16 -0500

    Renamed and cleaned up variables

--- Networking/AutobahnTwistedClient.py
+++ Networking/AutobahnTwistedClient.py
@@ -1,31 +1,29 @@
-import json, sys, time, threading, queue
+import json, sys, queue
 import Keys.Network as NETWORK
+
 import Keys.Settings as SETTINGS
 
-from Audio.AudioData import AudioData
-from Interfaces.Common.Task import Task, Simple
-
+from autobahn.twisted.websocket import WebSocketClientProtocol, WebSocketClientFactory
 from twisted.internet.protocol import ReconnectingClientFactory
-from autobahn.asyncio.websocket import WebSocketClientFactory, WebSocketClientProtocol
-
-# NETWORK.init()
+from twisted.internet import reactor
 
 
-global audio_queue, display_queue, remove_queue
-audio_queue = queue.Queue(3)
-display_queue = remove_queue = queue.Queue(25)
+class AutobahnTwistedClient:
+    def __init__(self, settings):
+        self.settings = settings
 
+        # log.startLogging(sys.stdout)
+
+        # TODO: Connect to PixelPi URL
+        self.autobahn_factory = MyClientFactory(u"ws://" + self.settings[SETTINGS.SERVER_IP_ADDRESS] + ":9000", settings)
+        self.autobahn_factory.protocol = MyClientProtocol
+
+    def run(self):
+        reactor.connectTCP(self.settings[SETTINGS.SERVER_IP_ADDRESS], 9000, self.autobahn_factory)
+        reactor.run()
 
 
 class MyClientProtocol(WebSocketClientProtocol):
-    client_settings = dict()
-    interface_list = list()
-
-    @classmethod
-    def setClassVariables(cls, client_settings, interface_list):
-        cls.client_settings = client_settings
-        cls.interface_list = interface_list
-
     def onConnect(self, response):
         print("Server connected: {0}".format(response.peer))
 
@@ -33,20 +31,8 @@ class MyClientProtocol(WebSocketClientProtocol):
         print("WebSocket connection open.")
 
         # Send server some basic details about the device upon connecting
-        #interface_descriptions = list(map(lambda interface: interface.getInterfaceJson(), interface_list))
-        
-        register_msg = self.client_settings
+        register_msg = self.factory.settings
         register_msg[NETWORK.COMMAND] = NETWORK.REGISTER_DEVICE
-##        register_msg = {
-##            NETWORK.COMMAND: NETWORK.REGISTER_DEVICE,
-##            SETTINGS.DEVICE: {
-##                SETTINGS.DESCRIPTION: client_settings[SETTINGS.DESCRIPTION]
-##            },
-##            SETTINGS.INTERFACE_LIST: interface_descriptions
-            
-##        }
-        
-        print(register_msg)
 
         self.sendMessage(json.dumps(register_msg, ensure_ascii=False).encode('utf8'))
 
@@ -61,31 +47,39 @@ class MyClientProtocol(WebSocketClientProtocol):
             if msg[NETWORK.COMMAND] == NETWORK.DISPLAY:
                 print("Display command!")
                 if msg[NETWORK.MODE] == NETWORK.AUDIO:
-                    if NETWORK.audio_queue.full():
-                        old_value_in_queue = NETWORK.audio_queue.get()
+                    if self.factory.audio_queue.full():
+                        old_value_in_queue = self.factory.audio_queue.get()
                         
-                    NETWORK.audio_queue.put(msg)
+                    self.factory.audio_queue.put(msg)
 
                 elif msg[NETWORK.MODE] == NETWORK.HOME:
-                    print("Put in display queue")
-                    NETWORK.display_queue.put(msg)
+                    # print("Put in display queue")
+                    self.factory.display_queue.put(msg)
                     
             elif msg[NETWORK.COMMAND] == NETWORK.REMOVE:
-                NETWORK.remove_queue.put(msg)
+                self.factory.remove_queue.put(msg)
 
     def onClose(self, wasClean, code, reason):
         print("WebSocket connection closed: {0}".format(reason))
-        #sys.exit()
+        self.factory.is_active = False
+        sys.exit(0)
         
         
 class MyClientFactory(WebSocketClientFactory, ReconnectingClientFactory):
+    def __init__(self, url, settings):
+        WebSocketClientFactory.__init__(self, url)
+        ReconnectingClientFactory.__init__(self)
 
-    protocol = MyClientProtocol
+        self.settings = settings
+        self.is_active = True
+
+        self.audio_queue = queue.Queue(2)
+        self.display_queue = self.remove_queue = queue.Queue(25)
 
     def clientConnectionFailed(self, connector, reason):
-        print("Client connection failed .. retrying ..")
-        self.retry(connector)
+        print('Connection failed. Reason:', reason)
+        ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
 
     def clientConnectionLost(self, connector, reason):
-        print("Client connection lost .. retrying ..")
-        self.retry(connector)
+        print('Lost connection. Reason:', reason)
+        ReconnectingClientFactory.clientConnectionLost(self, connector, reason)
